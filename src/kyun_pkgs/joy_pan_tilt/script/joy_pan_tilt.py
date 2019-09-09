7d97c269e8d34fd87cdfd828c3ef7cb4786e73dc
n2m2ng <info@nm2.jp>
2019-09-09 23:09:41 +0900

    add CMakeLists, package

--- src/kyun_pkgs/joy_pan_tilt/script/joy_pan_tilt.py
+++ src/kyun_pkgs/joy_pan_tilt/script/joy_pan_tilt.py
@@ -9,8 +9,13 @@ def callback(data):
     print 'buttons:["%s %s %s %s %s %s %s %s %s %s %s %s %s"]' % (data.buttons[0],data.buttons[1],data.buttons[2],data.buttons[3],data.buttons[4],data.buttons[5],data.buttons[6],data.buttons[7],data.buttons[8],data.buttons[9],data.buttons[10],data.buttons[11],data.buttons[12])
     print 'axes: ["%s %s %s %s %s %s"]' % (data.axes[0],data.axes[1],data.axes[2],data.axes[3],data.axes[4],data.axes[5])
 
-    pan.publish(data.axes[0]*3.14/2.0 )
-    tilt.publish(data.axes[1]*3.14/2.0)
+    pan_value = data.axes[0]*1024+1024
+    print pan_value
+    tilt_value = data.axes[1]*1024+1024
+    print tilt_value
+
+    pan.publish(pan_value)
+    tilt.publish(tilt_value)
 
 def listener():
     global pan
