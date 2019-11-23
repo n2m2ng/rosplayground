e5c56b8c6cc1a6fe5ca8bf10ac123b9d2fdb9260
Mitsukuni Sato <mitsukuni.sato@gmail.com>
2019-11-23 16:35:16 +0900

    removed build and devel directory

--- build/kyun_pkgs/beginner_tutorials/CMakeFiles/_run_tests_beginner_tutorials_roslaunch-check.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/_run_tests_beginner_tutorials_roslaunch-check.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- devel/share/realsense2_camera/cmake/realsense2_camera-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(realsense2_camera_MESSAGE_FILES "/home/kyun/rosplayground/src/realsense-ros/realsense2_camera/msg/IMUInfo.msg;/home/kyun/rosplayground/src/realsense-ros/realsense2_camera/msg/Extrinsics.msg")
-set(realsense2_camera_SERVICE_FILES "")
